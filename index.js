5a6687ad4b628bf64a99a4cab34182bf638c419d
Jannik Jost <jannik-jost@gmx.de>
2020-03-08 16:43:46 +0100

    basic jwt server

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const jwt = require("jsonwebtoken");
+
+const app = express();
+
+app.get("/api", (req, res) => {
+  res.json({});
+});
+app.post("/api/posts", verifyToken, (req, res) => {
+  jwt.verify(req.token, "secretkey", (err, authData) => {
+    if (err) {
+      res.sendStatus(403);
+    } else {
+      res.json({ message: "Post created", authData });
+    }
+  });
+});
+
+app.post("/api/login", (req, res) => {
+  // Mock user
+  const user = { id: 1, username: "user", email: "user@mail.com" };
+
+  jwt.sign({ user }, "secretkey", (err, token) => {
+    res.json({ token });
+  });
+});
+
+// FORMAT OF TOKEN
+// Authorization: Bearer <access_token>
+
+//Verify token
+function verifyToken(req, res, next) {
+  // Get auth header value
+  const bearerHeader = req.headers["authorization"];
+  if (bearerHeader) {
+    const bearer = bearerHeader.split(" ");
+    const token = bearer[1];
+    req.token = token;
+    next();
+  } else {
+    res.sendStatus(403);
+  }
+}
+
+app.listen(5000, () => console.log("server started on port 5000"));
